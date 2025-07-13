a037d40be391f0cfcc3a70348d317b8153f4dcec
Genda-phool <starkzain706@gmail.com>
2025-07-13 14:01:14 +0500

    change pass

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -7,9 +7,9 @@ resource "aws_db_instance" "laravel" {
   allocated_storage    = 20
   engine               = "mysql"
   engine_version       = "8.0"
-  instance_class       = "db.t3.micro"
-  username             = "admin"
-  password             = "admin1234"
+  instance_class       = "db.t2.micro"
+  username             = "Shadow"
+  password             = "Shadow12345"
   db_subnet_group_name = aws_db_subnet_group.default.name
   skip_final_snapshot  = true
   vpc_security_group_ids = [var.security_group_id]
