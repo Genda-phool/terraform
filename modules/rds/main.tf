ac71a6a68e234d4849a4a24bc280522c47beda6b
M Zain <159163782+Genda-phool@users.noreply.github.com>
2025-09-30 11:43:26 +0500

    Update main.tf

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -8,8 +8,8 @@ resource "aws_db_instance" "laravel" {
   engine               = "mysql"
   engine_version       = "8.0"
   instance_class       = "db.t2.micro"
-  username             = "Shadow"
-  password             = "Shadow12345"
+  username             = ".."
+  password             = ".."
   db_subnet_group_name = aws_db_subnet_group.default.name
   skip_final_snapshot  = true
   vpc_security_group_ids = [var.security_group_id]
